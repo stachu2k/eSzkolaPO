b1c30e7c1528dfbf414017d02d7171d564e56f68
Stachu <stachu@stachu.com>
2015-04-30 19:32:20 +0200

    pierwszy commit :)

---
+++ include/include.footer.php
@@ -0,0 +1,10 @@
+	</div>
+
+</div>
+
+<footer>
+	Projekt stworzono dla Politechnika Opolska. Grupa projektowa P7. Semestr pierwszy. Rok 2015.
+</footer>
+
+</body>
+</html>
---
+++ include/include.header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>e-Szkoła</title>
+<meta charset="utf-8" />
+<meta name="Description" content="System informatyczny e-Szkoła" />
+<meta name="Robots" content="ALL" />
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+
+<div id="container">
+
+	<header class="clearfix">
+		<img src="back-to-school.jpg" width="25%" height="25%" border="0" alt="back-to-school (24 kB)" style="float:left;"/>
+		<div class="logo_title">e-Szkoła</div>
+		<div class="logo_subtitle">Elektroniczny system zarządzania szkołą</div>
+	</header>
+
+	<div id="content">
